2c89f0e63147928f53b3e2b15c976dc4b4b15fbd
doingmov <yohei13111@mail.ru>
2026-02-23 20:35:58 +0300

    Refactor download_image and get_file_extension functions

--- utils.py
+++ utils.py
@@ -2,18 +2,16 @@ import os
 from urllib.parse import unquote, urlsplit
 import requests
 
-
 def download_image(url, path):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     headers = {"User-Agent": "Mozilla/5.0"}
-    try:
-        response = requests.get(url, headers=headers, timeout=10)
-        response.raise_for_status()
-        with open(path, "wb") as file:
-            file.write(response.content)
-        print(f"Сохранено: {path}")
-    except requests.RequestException:
-        print(f"Не удалось скачать {url}")
+    
+    response = requests.get(url, headers=headers, timeout=10)
+    response.raise_for_status()
+    with open(path, "wb") as file:
+        file.write(response.content)
+
+    print(f"Сохранено: {path}")
 
 
 def get_file_extension(url):
