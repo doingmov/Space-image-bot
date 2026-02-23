9f01640e10bc5c846641343db8ee92165a91725b
doingmov <yohei13111@mail.ru>
2026-02-23 20:35:11 +0300

    Refactor main.py with error handling and main function
    Refactor main.py to include error handling for API requests and use a main function.

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+import os
+from dotenv import load_dotenv
 from fetch_spacex_images import fetch_spacex_images
 from fetch_nasa_apod import fetch_nasa_apod_images
 from fetch_nasa_epic import fetch_epic_images
-import os
-from dotenv import load_dotenv
+import requests
 
-load_dotenv()
+def main():
+    load_dotenv()
+    nasa_api_key = os.environ['NASA_API_KEY']
 
-NASA_API_KEY = os.getenv("NASA_API_KEY")
+    try:
+        fetch_spacex_images(count=5)
+    except requests.RequestException:
+        print("Не удалось загрузить изображения SpaceX")
 
-fetch_spacex_images()
-fetch_nasa_apod_images(NASA_API_KEY, count=30)
-fetch_epic_images(NASA_API_KEY, count=10)
+    try:
+        fetch_nasa_apod_images(api_key=nasa_api_key, count=10)
+    except requests.RequestException:
+        print("Ошибка при загрузке APOD изображений. Пробуем DEMO_KEY…")
+        try:
+            fetch_nasa_apod_images(api_key="DEMO_KEY", count=10)
+        except requests.RequestException:
+            print("Сервис NASA APOD временно недоступен.")
+
+    try:
+        fetch_epic_images(api_key=nasa_api_key, count=5)
+    except requests.RequestException:
+        print("Ошибка при загрузке EPIC изображений. Пробуем DEMO_KEY…")
+        try:
+            fetch_epic_images(api_key="DEMO_KEY", count=5)
+        except requests.RequestException:
+            print("Сервис NASA EPIC временно недоступен.")
+
+if __name__ == "__main__":
+    main()
